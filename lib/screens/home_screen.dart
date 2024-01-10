38a655d27c9dff5c3c520efb512cff0aab4b6285
stp2003 <100586951+stp2003@users.noreply.github.com>
2024-01-10 10:43:30 +0530

    home screen made

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,55 @@
+import 'package:bhagwad_gita/functions/functions.dart';
+import 'package:bhagwad_gita/utils/custom_loader.dart';
+import 'package:bhagwad_gita/widgets/custom_app_bar.dart';
+import 'package:bhagwad_gita/widgets/custom_drawer.dart';
+import 'package:flutter/material.dart';
+
+import '../Models/Chapters_Model.dart';
+import '../widgets/AdhayListTile.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late List<GeetaChapters> _geetaChapters;
+  bool _loading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loading = true;
+    Functions.getGeetaChapters().then(
+      (geetaChapters) {
+        setState(() {
+          _geetaChapters = geetaChapters;
+          _loading = false;
+        });
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(loading: _loading),
+      drawer: const CustomDrawer(),
+      body: Container(
+        child: _loading
+            ? const CustomLoader(size: 60.0)
+            : ListView.builder(
+                itemCount: _geetaChapters.length,
+                itemBuilder: (context, index) {
+                  return AdhayListTile(
+                    geetaChapters: _geetaChapters,
+                    index: index,
+                  );
+                },
+              ),
+      ),
+    );
+  }
+}
